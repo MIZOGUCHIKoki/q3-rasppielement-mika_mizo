568dd16ca3a533af5a77b97b28797a42853e2750
MIKAMI shu <250372y@ugs.kochi-tech.ac.jp>
2022-12-13 10:10:10 +0900

    3.1-2

--- chap1/light.s
+++ chap1/light.s
@@ -1,14 +1,25 @@
-  .section .init
-  .global _start
+	.equ GPIO_BASE, 0x3f200000     
+	.equ GPFSEL0, 0x00             
+	.equ GPFSEL1, 0x04             
+	.equ GPFSEL2, 0x08
+	.equ GPSET0, 0x1c
+	.equ GPCLR0, 0x28
+
+	.equ GPFSEL_VEC1, 0x00000001
+	.equ LED_PORT, 10
+
+
+	.section .init
+	.global _start
 _start:
-  ldr     r0, =0x3f200000     @ GPIO 制御用の番地
+	ldr     r0, =GPIO_BASE     
 
-  @ GPIO #10 を出力用に，GPIO #11〜#19を入力用に設定
-  mov     r1, #0x00000001
-  str     r1, [r0, #0x04]
+	@ GPIO #10 を出力用に，GPIO #11〜#19を入力用に設定
+	ldr     r1, =GPFSEL_VEC1
+	str     r1, [r0, #GPFSEL1]
 
-    @ GPIO #10 に 1 を出力
-  mov     r1, #(1 << 10)
-  str     r1, [r0, #0x1C]
+	@ GPIO #10 に 1 を出力
+	mov     r1, #(1 << LED_PORT)
+	str     r1, [r0, #GPSET0]
 
-  loop:   b       loop
+loop:   b       loop
