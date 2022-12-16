cac2032f5e43bad1e7702d93873303e07e4840d2
MIZOGUCHI Koki <250373b@ugs.kochi-tech.ac.jp>
2022-12-16 06:15:41 +0000

    add col2

--- chap2/frame.s
+++ chap2/frame.s
@@ -67,6 +67,58 @@ rowRead:
 	colP:
 		mov	r1,	#(1 << COL1_PORT)
 		str	r1,	[r0,	#GPCLR0]		@	set "0" to COL
+	add	r5,	r5,	#4
+	row1:
+		and	r2,	r5,	r3
+		cmp	r2,	#0
+		beq	row2
+		mov	r1,	#(1 << ROW1_PORT)
+		str	r1,	[r0,	#GPSET0]
+	row2:
+		and	r2,	r5, r3,	lsl #1
+		cmp	r2,	#0
+		beq	row3
+		mov	r1,	#(1 << ROW2_PORT)
+		str	r1,	[r0,	#GPSET0]
+	row3:
+		and	r2,	r5, r3,	lsl #2
+		cmp	r2,	#0
+		beq	row4
+		mov	r1,	#(1 << ROW3_PORT)
+		str	r1,	[r0,	#GPSET0]
+	row4:
+		and	r2,	r5, r3,	lsl #3
+		cmp	r2,	#0
+		beq	row5
+		mov	r1,	#(1 << ROW4_PORT)
+		str	r1,	[r0,	#GPSET0]
+	row5:
+		and	r2,	r5, r3,	lsl #4
+		cmp	r2,	#0
+		beq	row6
+		mov	r1,	#(1 << ROW5_PORT)
+		str	r1,	[r0,	#GPSET0]
+	row6:
+		and	r2,	r5,	r3,	lsl	#5
+		cmp	r2,	#0
+		beq	row7
+		mov	r1,	#(1 << ROW6_PORT)
+		str	r1,	[r0,	#GPSET0]
+	row7:
+		and	r2,	r5, r3,	lsl #6
+		cmp	r2,	#0
+		beq	row8
+		mov	r1,	#(1 << ROW7_PORT)
+		str	r1,	[r0,	#GPSET0]
+	row8:
+		and	r2,	r5, r3,	lsl #7
+		cmp	r2,	#0
+		beq	colP
+		mov	r1,	#(1 << ROW8_PORT)
+		str	r1,	[r0,	#GPSET0]
+	colP:
+		mov	r1,	#(1 << COL2_PORT)
+		str	r1,	[r0,	#GPCLR0]		@	set "0" to COL
 loop:
 	b	loop
 clear:
