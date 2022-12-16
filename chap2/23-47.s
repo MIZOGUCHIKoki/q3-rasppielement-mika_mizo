03d36bef8badee5a9cae41da1b993b1e67535054
MIZOGUCHI Koki <250373b@ugs.kochi-tech.ac.jp>
2022-12-16 12:40:14 +0900

    executed successfully

--- chap2/23-47.s
+++ chap2/23-47.s
@@ -42,23 +42,36 @@ _start:
 	ldr	r1,	=GPFSEL_VEC2
 	str	r1,	[r0,	#GPFSEL0 + 8]
 	@ turn on the (r,c) LED <=> col==1 && row==0
-loop0:
 	bl clear
+loop0:
 	@ col3, row2
+	ldr			r2,	=0xfff
+cr32:
 	mov     r1, #(1 << COL3_PORT)
 	str     r1, [r0, #GPSET0]			@	COL3, set "1"
 	mov     r1, #(1 << ROW2_PORT)
 	str     r1, [r0, #GPCLR0]			@ ROW2,	set "0"
+	subs		r2,	r2,	#1
+	bne			cr32
 	@ Clear
 	mov			r1,	#(1 << COL3_PORT)
 	str			r1,	[r0,	#GPCLR0]
 	mov			r1,	#(1 << ROW2_PORT)
-	mov			r1,	[r0,	#GPSET0]
+	str			r1,	[r0,	#GPSET0]
 	@ col7,	row4
+	ldr			r2,	=0xfff
+cr74:
 	mov			r1,	#(1 << COL7_PORT)
 	str			r1,	[r0,	#GPSET0]
 	mov			r1,	#(1 << ROW4_PORT)
 	str			r1,	[r0,	#GPCLR0]
+	subs		r2,	r2,	#1
+	bne			cr74
+	@ Clear
+	mov			r1,	#(1 << COL7_PORT)
+	str			r1,	[r0,	#GPCLR0]
+	mov			r1,	#(1 << ROW4_PORT)
+	str			r1,	[r0,	#GPSET0]
 	b loop0
 clear:
 	mov     r1, #(1 << COL1_PORT)
