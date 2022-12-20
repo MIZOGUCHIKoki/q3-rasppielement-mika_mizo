e24404f4694b061c0d9a2167ade31e6fb8567c73
MIZOGUCHI Koki <250373b@ugs.kochi-tech.ac.jp>
2022-12-20 11:41:32 +0900

    edited

--- chap2/media.sh
+++ chap2/media.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
 USERNAME=$(whoami)
-#if [$USERNAME="koki"];
-#then
-#  echo "koki"
-#else 
-#	make $1.img
-#fi
         
 if [ $USERNAME = "koki" ]; then
   echo "MIZOGUCHI Koki on Mac"
-elif [ $USERNAME = "250372y" ]; then
+elif [ $USERNAME = "250372y" -o $USERNAME = "250373b" ]; then
+	make $1.img
   echo "MIKAMI/MIZOGUCHI on WS-Linux"
   cp $1.img /media/$USERNAME/boot/kernel7.img
 elif [ $USERNAME = "root" ]; then
   echo "MIZOGUCHI Koki on Docker-Linux"
 fi
-#make clean
+make clean
