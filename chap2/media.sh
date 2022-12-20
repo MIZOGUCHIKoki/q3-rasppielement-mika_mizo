99eedcaaa0ee50e9d38fabcb35c9f2e7444ae16f
MIZOGUCHI Koki <250373b@ugs.kochi-tech.ac.jp>
2022-12-20 02:20:39 +0000

    add whoami env

--- chap2/media.sh
+++ chap2/media.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-make $1.img
-if [ = koki] ; then
+USERNAME = $(whoami)
+if [ $USERNAME = koki] ; then
   echo "koki"
 else 
+	make $1.img
   cp $1.img /media/250372y/boot/kernel7.img
 fi
 make clean
