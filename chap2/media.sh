fc45193135f3b5a1566a2f27ace7cfd997ac8ae9
MIZOGUCHI Koki <250373b@ugs.kochi-tech.ac.jp>
2022-12-20 02:43:51 +0000

    changed syntax

--- chap2/media.sh
+++ chap2/media.sh
@@ -3,12 +3,12 @@
 USERNAME=$(whoami)
         
 if [ $USERNAME = "koki" ]; then
-  echo "MIZOGUCHI Koki on Mac"
+	echo -e "\e[31m MIZOGUCHI on Mac \e[m"
 elif [ $USERNAME = "250372y" -o $USERNAME = "250373b" ]; then
 	make $1.img
-  echo "MIKAMI/MIZOGUCHI on WS-Linux"
+	echo -e "\e[31m MIKAMI/MIZOGUCHI on WS-Linux \e[m"
   cp $1.img /media/$USERNAME/boot/kernel7.img
 elif [ $USERNAME = "root" ]; then
-  echo "MIZOGUCHI Koki on Docker-Linux"
+	echo -e "\e[31m MIZOGUCHI on Docker-Linux \e[m"
 fi
 make clean
