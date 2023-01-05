98740d0e36be4a2c3493b7986bf40666defbc623
MIZOGUCHI Koki <250373b@ugs.kochi-tech.ac.jp>
2023-01-05 03:05:48 +0000

    changed name and added comments

--- chap3/common.s
+++ chap3/common.s
@@ -1,12 +1,26 @@
 	.equ	GPIO_BASE,  0x3f200000 
+
 	.equ	GPFSEL0,    0x00       
+	.equ	GPFSEL1,		0x04
+	.equ	GPFSEL2,		0x08
+
 	.equ	GPSET0,     0x1C       
 	.equ	GPCLR0,     0x28       
 
-	.equ	GPFSEL_VEC0, 0x01201000 @ GPFSEL0 に設定する値 (GPIO #4, #7, #8 を出力用に設定)
-	.equ	GPFSEL_VEC1, 0x01249041 @ GPFSEL1 に設定する値 (GPIO #10, #12, #14, #15, #16, #17, #18 を出力用に設定)
-	.equ	GPFSEL_VEC2, 0x00209249 @ GPFSEL2 に設定する値 (GPIO #20, #21, #22, #23, #24, #25, #27 を出力用に設定)
+	@ GPFSEL0 Display
+	.equ	GPFSEL_VEC0, 0x01201000 
+	@ GPFSEL1 LED, Display
+	.equ	GPFSEL_VEC1, 0x01249041 
+	@ GPFSEL2 Display
+	.equ	GPFSEL_VEC2, 0x00209249 
 
+@ initialize stack pointer
+	.equ	STACK,			0x8000
+
+@ define port number
+	@ LED Light
+	.equ	LED_PORT,	10
+	@ LED Display
 	.equ	COL1_PORT, 27
 	.equ	COL2_PORT, 8
 	.equ	COL3_PORT, 25
@@ -23,5 +37,5 @@
 	.equ	ROW6_PORT, 20
 	.equ	ROW7_PORT, 7
 	.equ	ROW8_PORT, 16
-
-	.equ	STACK,			0x8000
+@ Timer base
+	.equ	TIMER_BASE,	0x3f003000
