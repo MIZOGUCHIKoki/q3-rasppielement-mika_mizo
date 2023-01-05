82c5fbcf868cdb2ce9b9979a468ad261ffa823d5
MIZOGUCHI Koki <250373b@ugs.kochi-tech.ac.jp>
2023-01-05 04:34:56 +0000

    changed used and reserved register

--- chap3/bit19.s
+++ chap3/bit19.s
@@ -3,7 +3,7 @@
 	.global _start
 
 @ Free regitser: r2
-@ Reserved register: r0, r1, r4
+@ Reserved register: r0, r1, r3, r4
 @ GPFSEL0 = 0x00, GPFSEL1 = 0x04, GPFSEL2 = 0x08
 
 _start:
@@ -17,8 +17,8 @@ _start:
 	mov	r4,	#(1 << LED_PORT)
 
 loop:
-	ldr		r2,	[r1, #GPFSEL1]
-	tst		r2,	#(1 << 19)
+	ldr		r3,	[r1, #GPFSEL1]
+	tst		r3,	#(1 << 19)
 	strne	r4,	[r0, #GPSET0]
 	streq	r4,	[r0, #GPCLR0]
 	b 	loop
