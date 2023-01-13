530347517be65973b2e211d4d2ec1f87ea14b702
MIZOGUCHI Koki <250373b@ugs.kochi-tech.ac.jp>
2023-01-13 02:06:01 +0000

    changed file pass and copied count99

--- chap4/media.sh
+++ chap4/media.sh
@@ -13,7 +13,7 @@ elif [ $USERNAME = "250372y" ]; then
   cp $1.img /media/$USERNAME/boot/kernel7.img
 elif [ $USERNAME = "koki" ]; then
 	echo -e "MIZOGUCHI on Mac"
-  docker cp ubuntu:/root/git/RasppiElement/chap3/$1.img ~/Documents/
+  docker cp ubuntu:/root/git/RasppiElement/chap4/$1.img ~/Documents/
   mv ~/Documents/$1.img ~/Documents/kernel7.img
   mv ~/Documents/kernel7.img /Volumes/boot/
   diskutil unmount boot
