837efcaf47ae1a934d9c868de5438290c05d5229
MIZOGUCHI Koki <250373b@ugs.kochi-tech.ac.jp>
2023-01-13 08:18:18 +0000

    executed successfully (flushing ??)

--- chap4/count100sec.s
+++ chap4/count100sec.s
@@ -1,6 +1,6 @@
 @------------------------------------
-@ target time = r7
-@ not Reserved register = r0, r6, r7
+@ target time = r7,	r1
+@ not Reserved register = r0, r6
 @
 @ while(true) {
 @	 if (target time < Current time) {
@@ -9,26 +9,33 @@
 @ dislpay;
 @ }
 @------------------------------------
-	.equ			count,	1000000
+	.equ			count,	1000000		@ 1sec
+	.equ			dpr,		1000
 	.include	"common.s"
 	.section 	.init
 	.global 	_start
 _start:
 	mov	sp,		#STACK					@ initalize stack pointer
 	ldr	r1,		=frame_buffer
+	push	{r1}
 	ldr r9,		=TIMER_BASE
 	ldr	r5,		=nb_all
 @ initialize target time
 	ldr	r7,		[r9, #GPFSEL1]
+	ldr	r0,		=count
+	add	r7,		r0,	r7
+	mov	r0,		#dpr
+	add	r1,		r0,	r7
 @ setting loop variable
 	mov	r11,	#0
 	mov	r12,	#0
+	mov	r4,		#0
 loop:
 @ Task: [ Struct strings ]
 writeFrame_buffer:
 	ldr		r6,		[r9, #GPFSEL1]	@ r6 current
 	cmp		r6,		r7			@	Current, Target
-	bcc		endp					@ Currnet < Target
+	bcc		disp					@ Currnet < Target
 	ldr		r0,		=count	
 	add		r7,	r0,	r7	@ update target time
 	@ Write Process
@@ -39,21 +46,37 @@ writeFrame_buffer:
 	multi:
 		ldrb	r6,		[r8,	r2]
 		lsl		r6,		#4
-		ldrb	r4,		[r3,	r2]			@ r4 = nb_0[r2]		read only 1Byte
-		orr		r7,		r4,		r6
+		ldrb	r0,		[r3,	r2]			@ r0 = nb_0[r2]		read only 1Byte
+		pop		{r1}
+		push	{r7}
+		orr		r7,		r0,		r6
 		strb	r7,		[r1,	r2]			@ store
+		pop		{r7}
+		push	{r1}
 		subs	r2,		r2,		#1
 		bpl		multi								@ 0以上
 	
-	addeq	r11,	r11,	#1	
+	add		r11,	r11,	#1	
 	cmp		r11,	#10
 	moveq	r11,	#0
 
 	addeq	r12,	r12,	#1
 	cmp		r12,	#10
 	moveq	r12,	#0
+@ Task: [ display ]
+disp:
+	ldr		r6,		[r9, #GPFSEL1]	@ r6 current
+	cmp		r6,		r1			@	Current, Target
+	bcc		endp					@ Currnet < Target
+	mov		r0,		#dpr	
+	add		r1,		r0,	r1	@ update target time
+	add		r4,		r4,	#1
+	cmp		r4,		#8
+	moveq	r4,		#0
+
+	bl	display_row
+
 endp:
-	bl	display
 	b	loop
 @	----- data base -----
 nb_0:
@@ -84,4 +107,4 @@ nb_all:
 	.section	.data
 	.global		frame_buffer
 frame_buffer:
-	.byte 0x00,	0x00,	0x00,	0x00,	0x00,	0x00,	0x00,	0x00
+	.byte 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,0x0, 0x0
