99d5a61fb99753c8408376478933d893a318565f
MIKAMI shu <250372y@ugs.kochi-tech.ac.jp>
2023-01-16 17:52:22 +0900

    completed

--- chap4/bar.s
+++ chap4/bar.s
@@ -24,52 +24,54 @@ _start:
 	ldr	r4,	[r3, #GPFSEL1]
 
 	ldr	r5,	=waite
-	ldr	r7,	=port
-	ldr	r1,	=on
-	mov 	r8,	#0
-	
-loop0:
-	ldr	r9, 	[r3, #GPFSEL1]
-loop1:
-	ldr	r5,	=waite
-	ldr	r12,	[r5, r8, lsl #2]
-	add	r10,	r4,	r12
-	
-	cmp	r10,	r9
-	addcs	r6, 	r12,	r2
-	strcs	r6,	[r5, r8, lsl #2]
-	
-	bcs	cmp
-	b	next
-	
-cmp:
-	mov	r10,	#1
-	ldr	r5,	[r7, r8]
-	lsl	r10,	r10,	r5
-	ldr	r11,	[r1, r8]
-	cmp	r11,	#0
-	strne	r10,	[r0, #GPSET0]
-	streq	r10,	[r0, #GPCLR0]
+	ldr	r6,	=port
 
-next:	
-	add	r8,	r8,	#1
-	cmp	r8, 	#16
-	bne	loop1
-	moveq	r8, 	#0
-	ldreq	r4,	[r3, #GPFSEL1]
-	beq	loop0
+	mov	r7,	#15
+	mov	r12,	#1
 	
-loop:	b	loop
-	
-	
+set:
+	ldr	r8,	[r5, r7, lsl #2]
+	add	r8,	r8,	r4
+	str	r8,	[r5, r7, lsl #2]
+	subs	r7, 	r7,	#1
+	bpl	set
+
+loop0:
+	mov	r7,  	#15
+	ldr	r8,	[r3, #GPFSEL1]
+
+loop1:
+	ldr	r9,	[r5, r7, lsl #2] @目標時刻
+	cmp	r8,	r9
+	bmi	endp
+
+	add	r9,	r9,	r2
+	str	r9,	[r5, r7, lsl #2]
+	and	r10,	r7, 	#7
+	ldrb	r11,	[r6, r10]
+	lsl	r11,	r12,	r11
+
+	mov	r10,	r7, lsr #3
+	cmp	r10,	#1
+	bmi	on
+
+off:
+	str	r11,	[r0, #GPCLR0]
+	b	endp
+
+on:
+	str	r11,	[r0, #GPSET0]
+
+endp:
+	subs	r7,	r7,	#1
+	bpl	loop1
+	b loop0
+
 
 	.section	.data
 
 waite:
-	.word	0x00, 0x3e80, 0x13880, 0x2ee00,	0x55f00, 0x88b80, 0xc7380, 0x1117000,0x2625a0, 0x25e720, 0x24ed20, 0x2337a0, 0x20c6a0, 0x1d9a20, 0x19b220, 0x150ea0
+	.word	0x00, 0x3e80, 0x13880, 0x2ee00,	0x55f00, 0x88b80, 0xc7380, 0x111700,0x2625a0, 0x25e720, 0x24ed20, 0x2337a0, 0x20c6a0, 0x1d9a20, 0x19b220, 0x150ea0
 
 port:
-	.byte 	27, 8, 25, 23, 24, 22, 17, 4, 27, 8, 25, 23, 24, 22, 17, 4
-on:
-	.byte	1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0
-	
+	.byte 	27, 8, 25, 23, 24, 22, 17, 4
