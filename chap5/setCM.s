05efe3fb242e60f82aa0c117a99c4b672753f6ea
MIZOGUCHI Koki <250373b@ugs.kochi-tech.ac.jp>
2023-01-18 01:56:31 +0000

    add set in/out port mnemonics

--- chap5/setCM.s
+++ chap5/setCM.s
@@ -3,6 +3,14 @@
 	.global		setCM
 setCM:
 	push	{r0, r1}
+	ldr	r0,	=GPIO_BASE
+	ldr	r1, =GPFSEL_VEC0
+	str	r1, [r0, #GPFSEL0 + 0]
+	ldr	r1, =GPFSEL_VEC1
+	str	r1, [r0, #GPFSEL0 + 4]
+	ldr	r1, =GPFSEL_VEC2
+	str	r1, [r0, #GPFSEL0 + 8]
+
 	ldr	r0, =CM_BASE
 	ldr	r1, =0x5a000021
 	str	r1, [r0, #CM_PWMCTL]
