338b7ed4f0586336640a9dbc5a0ca0cb62969edf
MIZOGUCHI Koki <250373b@ugs.kochi-tech.ac.jp>
2023-01-18 13:48:56 +0000

    led on or off

--- chap5/debug.s
+++ chap5/debug.s
@@ -4,26 +4,21 @@
 @ 	Turn LED ON momentarily
 	.include	"common.s"
 	.section	.text
-	.global		debug_on, debug_momentarily
-debug_on:
-	push	{r0 - r2, r14}
-	ldr		r0,	=GPIO_BASE
-	mov		r1,	#(1 << 10)
-	ldr		r2,	=0xfff
-	str		r1,	[r0, #GPSET0]
-loop:
-	subs	r2,	r2,	#1
-	bne		loop
-	str		r1,	[r0, #GPCLR0]
-	pop		{r0 - r2, r14}
-	bx		r14
-
-debug_momentarily:
-	push	{r0 - r2, r14}
+	.global		led_on, led_off
+led_on:
+	push	{r0 - r1, r14}
 	bl		debug_setting
 	mov		r1,	#(1 << 10)
 	str		r1,	[r0, #GPSET0]
-	pop		{r0 - r2, r14}
+	pop		{r0 - r1, r14}
+	bx		r14
+
+led_off:
+	push	{r0 - r1, r14}
+	bl		debug_setting
+	mov		r1,	#(1 << 10)
+	str		r1,	[r0, #GPCLR0]
+	pop		{r0 - r1, r14}
 	bx		r14
 
 debug_setting:
