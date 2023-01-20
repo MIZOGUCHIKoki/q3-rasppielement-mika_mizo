45bdd6d42527aa3817971525ddcf5df694f368a9
MIKAMI shu <250372y@ugs.kochi-tech.ac.jp>
2023-01-20 12:34:01 +0900

    improvement music.s

--- chap5/sound.s
+++ chap5/sound.s
@@ -1,63 +1,67 @@
-	@r2:sound
-	@r3:onp
-
-	.equ	sec1,	1000000
-	.equ	sec025,	250000
-
 	.include	"common.s"
-	.section	.text
-	.global		sound
-sound:
-	push	{r0 - r8, r14}
+	.section	.init
+	.global		_start
+_start:
+	mov 	sp,	#STACK
 
-	@set GPIO
-	ldr	r0,	=GPIO_BASE
-	ldr	r1, 	=GPFSEL_VEC0
-	str	r1,	[r0, #GPFSEL0 + 0]
-	ldr	r1,	=GPFSEL_VEC1
-	str	r1,	[r0, #GPFSEL0 + 4]
-	ldr	r1, 	=GPFSEL_VEC2
-	str	r1,	[r0, #GPFSEL0 + 8]
+	bl	settings
 
-	@set CM
-	ldr	r0, =CM_BASE
-	ldr	r1, =0x5a000021
-	str	r1, [r0, #CM_PWMCTL]
-1:
-	ldr	r1, [r0, #CM_PWMCTL]
-	tst	r1, #0x80
+	ldr	r0,	=TIMER_BASE
+	ldr	r1,	[r0, #GPFSEL1]
+	ldr	r2,	=soundData
+	ldr	r3,	=sound_longData
+	ldr	r4,	=sound
+	ldr	r5,	=sound_long
+
+	mov	r6,	#0
+
+	ldr	r12,	=PWM_BASE
+
+loop0:
+	ldrb	r7,	[r4, r6]
+	ldrb	r8,	[r5, r6]		
+	ldr	r9,	[r2, r7, lsl #2]	@sound
+	ldr	r10,	[r3, r8, lsl #2]	@sound_long
+	add	r1,	r1,	r10
+
+	cmp	r9, 	#0
+	streq	r9,	[r12, #PWM_DAT2]
+
+	strne	r9,	[r12, #PWM_RNG2]
+	lsrne	r9,	r9, 	#1
+	strne	r9,	[r12, #PWM_DAT2]
+
+loop1:
+	ldr	r11,	[r0, #GPFSEL1]
+	cmp	r1,	r11
+	bcs	loop1
+
+	mov	r7,	#0
+	str	r7,	[r12, #PWM_DAT2]
+	ldr	r7,	=0xffff
+
+1:	subs	r7,	r7,	#1
 	bne	1b
-	ldr	r1, =(0x5a000000 | (2 << 12))
-	str	r1, [r0, #CM_PWMDIV]
-	ldr	r1, =0x5a000211
-	str 	r1, [r0, #CM_PWMCTL]
-	
-	@set PWM
-	ldr	r0,	=PWM_BASE
-	ldr	r1,	=(1 << PWM_PWEN2 | 1 << PWM_MSEN2)
-	str	r1,	[r0, #PWM_CTL]
 
-	@set time
-	ldr	r4,	=TIMER_BASE
-	ldr	r5,	[r4, #GPFSEL1] @Start time
-	ldr	r6,	=sec025
+	add	r6,	r6,	#1
+	cmp	r6,	#14
+	moveq	r6,	#0
+	b	loop0
 
-	ldr	r9,	=sound_buffer
-	mov	r10, 	#0
-
-loop00:
-	ldr	r7,	[r4, #GPFSEL1]
-	cmp	r5,	r7
-	addcc	r5,	r5,	r6
-	ldrbcc	r8,	[r9, r10]
-	
+loop:	b	loop
 	
 	
 
+
+	.section	.data
+soundData:
+	.word	18355, 16354, 14567, 13753, 12244, 10909, 0
+
+sound_longData:
+	.word	500000, 250000
+
+sound:
+	.byte	0,6, 0,6, 4,6, 4,6, 5,6, 5,6, 4
+sound_long:
+	.byte	1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 0
 	
-
-
-
-
-	pop	{r0 - r8, r14}
-	bx	r14
